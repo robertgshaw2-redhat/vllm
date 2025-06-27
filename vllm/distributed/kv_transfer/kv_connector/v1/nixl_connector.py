835546e170cd1de43a7bc806ab71c6bde216c621
Pravein Govindan Kannan <pravein.govindan.kannan@ibm.com>
2025-06-27 17:05:44 +0530

    Release handle

--- vllm/distributed/kv_transfer/kv_connector/v1/nixl_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/nixl_connector.py
@@ -839,7 +839,7 @@ class NixlConnectorWorker:
             for handle, xfer_stime in handles:
                 xfer_state = self.nixl_wrapper.check_xfer_state(handle)
                 if xfer_state == "DONE":
-                    #self.nixl_wrapper.release_xfer_handle(handle)
+                    self.nixl_wrapper.release_xfer_handle(handle)
                     del transfers[req_id]
                 elif xfer_state == "PROC":
                     done_req_ids.add(req_id)
